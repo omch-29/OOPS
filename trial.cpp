8195e68af953629740c2e24018e20570aae54e4b
omch-29 <chandwaniom29@gmail.com>
2026-03-21 13:40:58 +0530

    users

--- trial.cpp
+++ trial.cpp
@@ -14,33 +14,33 @@ public:
 
 class UserService {
 private:
-    unordered_map<int, User*> users;
+    unordered_map<int, User> users;
 
 public:
-    void addUser(User* user) {
-        users[user->id] = user;
+    void addUser(const User& user) {
+        users.emplace(user.id, user);
     }
 
-    User* getUser(int id) {
+    User& getUser(int id) {
         if(users.find(id) != users.end())
-            return users[id];
-        return nullptr;
+            return users.at(id);
+        throw runtime_error("User not found");
     }
 };
 int main(){
     UserService us;
-    User* u1 = new User(1,"abc");
-    User* u2 = new User(2, "cde");
+    User u1(1,"abc");
+    User u2(2, "cde");
 
     us.addUser(u1);
     us.addUser(u2);
 
-    User* user = us.getUser(1);
 
-    if(user != nullptr) {
-        cout << "User found: " << user->name << endl;
-    } else {
-        cout << "User not found" << endl;
+   try {
+        User& user = us.getUser(1);
+        cout << "User found: " << user.name << endl;
+    } catch(exception& e) {
+        cout << e.what() << endl;
     }
 
     return 0;
